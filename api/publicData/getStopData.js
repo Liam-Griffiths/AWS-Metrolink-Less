3612ff04c27481d60fa4276a09ab3e63a8b5f10f
Liam Griffiths <liam@liam-griffiths.co.uk>
2019-08-07 15:52:12 +0100

    countdown accurate wait added

--- api/publicData/getStopData.js
+++ api/publicData/getStopData.js
@@ -16,9 +16,45 @@ module.exports.getStopData = (event, context, callback) => {
 
             docClient.get(params, function (err, data) {
                 if (err) {
-                    callback(null, { statusCode: 500, body: JSON.stringify({status: "error", message:"nothing found", data: null}) });
+                    callback(null, { statusCode: 500, body: JSON.stringify({ status: "error", message: "nothing found", data: null }) });
                 } else {
-                    callback(null, { statusCode: 200, body: JSON.stringify({status: "success", message:"success", data: data.Item}) });
+
+                    var stopData = JSON.parse(data.Item.data);
+                    var fetchTime = data.Item.fetchTime;
+                    var timeNow = Date.now()
+
+                    // Finally modulo in a real use case! I'm so happy.
+                    var timeDiff = timeNow - fetchTime;
+                    var diffMins = Math.floor(timeDiff / 60000);
+                    var diffSecs = ((timeDiff % 60000) / 1000).toFixed(0);
+
+                    stopData.forEach(element => {
+
+                        var waitMins = +element.wait;
+                        var waitMins = (waitMins - diffMins) - 1;
+                        var waitSecs = 60 - diffSecs;
+
+                        if (waitMins < 0) {
+                            if (element.status = "Due") {
+                                element.status = "Departed";
+                            }
+                            element.wait = waitMins;
+                            element.seconds = 0;
+                        }
+                        else if (waitMins == 0) {
+                            if (element.status = "Due") {
+                                element.status = "Due Now";
+                            }
+                            element.wait = waitMins;
+                            element.seconds = waitSecs;
+                        } else {
+                            element.wait = waitMins;
+                            element.seconds = waitSecs;
+                        }
+
+                    });
+                    data.Item.data = stopData;
+                    callback(null, { statusCode: 200, body: JSON.stringify({ status: "success", message: "success", data: data }) });
                 }
             });
         } catch (error) {
